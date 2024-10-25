1ca86aa9005d75e6adb6b77a706122829359724e
very97 <very97@naver.com>
2024-10-25 21:21:42 +0900

    updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
 // console.log("proxy", BACKEND_PROXY);
 const api = axios.create({
   //baseURL: LOCAL_BACKEND,
-  baseURL: PROD_BACKEND + "api",
+  baseURL: PROD_BACKEND + "/api",
   headers: {
     "Content-Type": "application/json",
     authorization: `Bearer ${sessionStorage.getItem("token")}`,
